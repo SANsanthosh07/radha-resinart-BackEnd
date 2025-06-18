d60ac80c152977d72a8c420b4fb722ad06324add
santhosh <santhoshravi769@gmail.com>
2025-06-18 19:30:06 +0530

    add port

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-const app = require("./app");
 const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const getTouchs = require("./routes/getTouchRoute");
+
 dotenv.config();
 
 const database = require("./database");
 database();
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Good" });
+});
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(express.json({ limit: "4mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use("/api/v1/", getTouchs);
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
