c263c5d7392b18e5d48277df10366a68273ca912
santhosh <santhoshravi769@gmail.com>
2025-06-19 13:53:36 +0530

    added logger files

--- controllers/getTouchController.js
+++ controllers/getTouchController.js
@@ -1,20 +1,25 @@
 const GetTouch = require("../models/getTouch");
+const logger = require("../utils/logger");
 
 const handlePostGetTouch = async (req, res) => {
   try {
+    logger.info("Receving data from new user");
     const { name, email, subject, message } = req.body;
-
+    logger.info("Passed all required condition");
     if (!name || !email || !subject || !message) {
       return res.status(400).json({ message: "All fields are required" });
     }
+    logger.info("Data stored in MongoDB");
     const newEntryGetTouch = await GetTouch.create({
       name,
       email,
       subject,
       message,
     });
+    logger.info("Data received successfully");
     res.status(200).json({ data: newEntryGetTouch });
   } catch (error) {
+    logger.error("Error while receving data");
     console.log("Error while fetching getTouch Details");
     res.status(500).json({ message: "Server Error", error: error.message });
   }
---
+++ utils/logger.js
@@ -0,0 +1,18 @@
+const { createLogger, transports, format } = require("winston");
+
+const logger = createLogger({
+  transports: [
+    new transports.Console({
+      level: "info",
+      format: format.combine(
+        format.colorize({ all: true }),
+        format.timestamp({ format: "DD-MM-YYYY HH-mm-ss" }),
+        format.printf((info) => {
+          const { timestamp, level, message } = info;
+          return `${timestamp}-[${level}]= ${message}`;
+        })
+      ),
+    }),
+  ],
+});
+module.exports = logger;
