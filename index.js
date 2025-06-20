cf3243590eea6e3925198ba76149cfd03afb27dc
santhosh <santhoshravi769@gmail.com>
2025-06-20 19:33:45 +0530

    removed  utils backend

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Logger = require("../BackEnd/utils/logger");
+const Logger = require("./utils/logger");
 const app = require("./app");
 const dotenv = require("dotenv");
 const chalk = require("chalk");
